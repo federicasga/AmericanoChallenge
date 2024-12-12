d402924b1b107b9d2a97846bdc21fb2ed21ac9ad
federicasga <fsgammato24@fed.idserve.net>
2024-12-12 15:24:25 +0100

    Initial Commit

---
+++ AmericanoChallenge/AmericanoChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmericanoChallengeApp.swift
+//  AmericanoChallenge
+//
+//  Created by Federica Sgammato on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AmericanoChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
